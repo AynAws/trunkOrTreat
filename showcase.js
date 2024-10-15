c5a14e20b34d1b4c8fed79a1623f6d1bad9db87b
GrimCreeperyt <grimcreeperyt@gmail.com>
2024-10-15 19:56:47 -0400

    done

---
+++ showcase.js
@@ -0,0 +1,11 @@
+if (window.innerWidth >= 576) {
+    $(document).ready(function () {
+      $('img').on('mouseover', function (event) {
+        $(event.currentTarget).siblings('.text-box').fadeIn();
+      });
+  
+      $('img').on('mouseout', function (event) {
+        $(event.currentTarget).siblings('.text-box').fadeOut();
+      });
+    });
+  }
