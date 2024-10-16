939161bf046b2abc84cb19dd4bb27ddd4e149ecf
Ayaan Awais <105724871+AynAws@users.noreply.github.com>
2024-10-16 10:51:34 -0400

    Update showcase.js

--- showcase.js
+++ showcase.js
@@ -8,4 +8,8 @@ if (window.innerWidth >= 576) {
         $(event.currentTarget).siblings('.text-box').fadeOut();
       });
     });
-  }
+  } else {
+    $(document).ready(function () {
+        $('.text-box').fadeIn();
+    }
+}
